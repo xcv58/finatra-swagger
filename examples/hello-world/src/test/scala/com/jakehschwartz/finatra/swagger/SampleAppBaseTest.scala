88e04c1d8f1345bda2a8cd08317c01d9d082fa64
Matthew Dickinson <matt+github@alpha345.com>
2019-12-15 17:13:53 -0700

    comment in test base

--- examples/hello-world/src/test/scala/com/jakehschwartz/finatra/swagger/SampleAppBaseTest.scala
+++ examples/hello-world/src/test/scala/com/jakehschwartz/finatra/swagger/SampleAppBaseTest.scala
@@ -15,6 +15,9 @@ trait SampleAppBaseTest extends FeatureTest with Matchers {
   def stage: Stage = Stage.DEVELOPMENT
   override def server: EmbeddedHttpServer
 
+  // see https://twitter.github.io/finatra/user-guide/testing/feature_tests.html#sharing-a-server-fixture-between-many-feature-tests
+  // for best practices in sharing a server fixture between tests
+
   protected def makeServer(serverName: String): EmbeddedHttpServer = new EmbeddedHttpServer(
     twitterServer = new SampleApp { override val name: String = serverName },
     stage = stage
